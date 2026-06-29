0e5c9aa13dfb7915e94f65e6e2b98bcfe465ed97
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-06-29 23:34:38 +0530

    Practicing Loops

---
+++ Practice/Loops_Practice/script.js
@@ -0,0 +1,21 @@
+/*
+
+Print numbers from 1 to 100. 
+
+*/
+
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+}
+
+/*
+
+Print even numbers from 1 to 100. 
+
+*/
+
+for(let i = 1; i <= 100; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
