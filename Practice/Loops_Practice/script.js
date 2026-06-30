b30f7a8b40a41b4d4ce934d455599507c9bd4cfe
Dhron Yadav <dhronyadavdhronyadav@gmail.com>
2026-06-30 23:24:55 +0530

    Practicing Loops 02

--- Practice/Loops_Practice/script.js
+++ Practice/Loops_Practice/script.js
@@ -19,3 +19,27 @@ for(let i = 1; i <= 100; i++){
         console.log(i);
     }
 }
+
+/*
+
+Find the sum of numbers from 1 to 10.
+
+*/
+
+let sum = 0;
+for(let i = 1; i <= 10; i++){
+    sum += i;
+}
+console.log(sum);
+
+/*
+
+Find the factorial of 5.
+
+*/
+
+let fact = 1;
+for(let i = 1; i <= 5; i++){
+    fact *= i;
+}
+console.log(fact);
