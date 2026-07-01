99e7489900d342800e466f86b7e52dadbf422e11
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-07-01 23:00:08 +0530

    Practicing Loops 03

--- Practice/Loops_Practice/script.js
+++ Practice/Loops_Practice/script.js
@@ -4,8 +4,8 @@ Print numbers from 1 to 100.
 
 */
 
-for(let i = 1; i <= 100; i++){
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
 }
 
 /*
@@ -14,10 +14,10 @@ Print even numbers from 1 to 100.
 
 */
 
-for(let i = 1; i <= 100; i++){
-    if(i % 2 === 0){
-        console.log(i);
-    }
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
 
 /*
@@ -27,8 +27,8 @@ Find the sum of numbers from 1 to 10.
 */
 
 let sum = 0;
-for(let i = 1; i <= 10; i++){
-    sum += i;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
 }
 console.log(sum);
 
@@ -39,7 +39,35 @@ Find the factorial of 5.
 */
 
 let fact = 1;
-for(let i = 1; i <= 5; i++){
-    fact *= i;
+for (let i = 1; i <= 5; i++) {
+  fact *= i;
 }
 console.log(fact);
+
+/*
+
+Print the multiplication table of 5. 
+
+*/
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`5 X ${i} = ${5 * i}`);
+}
+
+/*
+
+Find the largest number in an array. 
+
+*/
+
+const numbers = [5, 12, 3, 20, 8];
+
+let largest = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > largest) {
+    largest = numbers[i];
+  }
+}
+
+console.log(largest);
