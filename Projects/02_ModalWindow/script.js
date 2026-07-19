c8f98cc9cbbd4d618cad34a52945bfd296f40ef3
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-07-19 14:55:58 +0530

    ModalWindow Complete

--- Projects/02_ModalWindow/script.js
+++ Projects/02_ModalWindow/script.js
@@ -1,1 +1,28 @@
-"use strict"
+"use strict";
+
+const btns = document.querySelectorAll(".btn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".closebtn");
+const backArea = document.querySelector(".div2");
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  backArea.classList.add("hidden");
+};
+
+for (let btn of btns) {
+  btn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    backArea.classList.remove("hidden");
+  });
+}
+
+closeBtn.addEventListener("click", closeModal);
+
+backArea.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
