9e71d3100971c496cbdaff3b723662f326936d15
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-08-06 07:20:23 +0530

    Practice - String Methods 04

--- Practice/Operators_&_Strings_Practice/script.js
+++ Practice/Operators_&_Strings_Practice/script.js
@@ -930,3 +930,58 @@ function logBookTheme(title) {
     console.log(`This book is about ${message}`);
   }
 }
+
+/* 
+  Q.8 Below is the bookCategories variable that stores a string of categories. Each category is separated with a semicolon, for example, in a string "science;computing", 'science' and 'computing' are separate categories.
+
+  Write a function called logBookCategories that takes a string of categories separated with semicolons, and logs each category to the console (as separate strings).
+*/
+
+const bookCategories =
+  "science;computing;computer science;algorithms;business;operating systems;networking;electronics";
+
+function logBookCategories(categories) {
+  for (let cat of categories.split(";")) {
+    console.log(cat);
+  }
+}
+
+logBookCategories(bookCategories);
+
+/* 
+  Q.9 Now, the opposite. Each book from the books array has the keywords property.
+
+  Write a function called getKeywordsAsString that takes the books array as an argument, collects keywords from each book, removes duplicates, and then joins them to create a single string where keywords are separated by a semicolon.
+*/
+
+function getKeywordsAsString(books) {
+  let allKeywords = [];
+  for (let book of books) {
+    allKeywords.push(...book.keywords);
+  }
+
+  allKeywords = [...new Set(allKeywords)].join(";");
+  return allKeywords;
+}
+
+/* 
+  Q.10 Below is the bookChapters array that contains inner arrays. Each inner array consists of a chapter's title, and the number of a page, for example, in ['The Basics', 14], 'The Basics' is the chapter's title, and 14 is the number of a page.
+
+  Write a function called logBookChapters that takes an array of arrays (like bookChapters) as an argument, and logs each chapter's name to the console together with the page number. The page number should be separated from the chapter's name with underscores (take a look at the example below).
+*/
+
+const bookChapters = [
+  ["The Basics", 14],
+  ["Sorting", 254],
+  ["Searching", 372],
+  ["Graphs", 526],
+  ["Strings", 706],
+];
+
+function logBookChapters(bookChapters){
+  for(const [title, pages] of bookChapters){
+    console.log(title.padEnd(20,'_'), pages);
+  }
+}
+
+logBookChapters(bookChapters);
