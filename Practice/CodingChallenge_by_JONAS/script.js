d2af0e494217da13e58ffe8ade0ab4e0f9f26293
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-08-07 14:49:05 +0530

    Practice - Challenge by Jonas(Udemy) 03

--- Practice/CodingChallenge_by_JONAS/script.js
+++ Practice/CodingChallenge_by_JONAS/script.js
@@ -37,7 +37,7 @@ document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
 const btn = document.querySelector("button");
 const textArea = document.querySelector("textArea");
-btn.textContent = "Click Me";
+// btn.textContent = "Click Me";
 
 //Solution 1
 
@@ -72,7 +72,9 @@ function toCamelCase2() {
   textArea.value = "";
 }
 
-btn.addEventListener("click", toCamelCase2);
+
+//------ Now This button is being used for next challenge ---------//
+// btn.addEventListener("click", toCamelCase2);
 
 /* 
 
@@ -92,11 +94,75 @@ const flights =
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-function normalizeFlights(flights){
-  for(let flight of flights.split('+')){
-    const [type, from, to, time] = flight.split(';');
-    console.log(`${type.includes('_Delayed')? "🔴": ''}${type.replaceAll('_'," ")} from ${from.toUpperCase()} to ${to.slice(0,3).toUpperCase()} ${time.replace(':',"h")}`.padStart(55,));
+function normalizeFlights(flights) {
+  for (let flight of flights.split("+")) {
+    const [type, from, to, time] = flight.split(";");
+    console.log(
+      `${type.includes("_Delayed") ? "🔴" : ""}${type.replaceAll("_", " ")} from ${from.toUpperCase()} to ${to.slice(0, 3).toUpperCase()} ${time.replace(":", "h")}`.padStart(
+        55,
+      ),
+    );
   }
-
 }
 normalizeFlights(flights);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button.
+
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+
+*/
+
+
+const poll = {
+  question: "What is your favorite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const input = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option numbers)`));
+    typeof input === 'number' && input < this.answers.length && this.answers[input]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array'){
+    type === 'array' && console.log(this.answers);
+    type === 'string' && console.log(`Poll results are ${this.answers.join(", ")}`);
+  }
+};
+
+btn.addEventListener("click", poll.registerNewAnswer.bind(poll));
+poll.displayResults.call({answers: [5, 2, 3]});
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string');
