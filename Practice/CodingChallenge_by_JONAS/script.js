87d727c9c03261e714b021350ec9819142cfb8f5
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-08-07 12:30:28 +0530

    Practice - Challenge by Jonas(Udemy) 02

--- Practice/CodingChallenge_by_JONAS/script.js
+++ Practice/CodingChallenge_by_JONAS/script.js
@@ -73,3 +73,30 @@ function toCamelCase2() {
 }
 
 btn.addEventListener("click", toCamelCase2);
+
+/* 
+
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+*/
+
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+function normalizeFlights(flights){
+  for(let flight of flights.split('+')){
+    const [type, from, to, time] = flight.split(';');
+    console.log(`${type.includes('_Delayed')? "🔴": ''}${type.replaceAll('_'," ")} from ${from.toUpperCase()} to ${to.slice(0,3).toUpperCase()} ${time.replace(':',"h")}`.padStart(55,));
+  }
+
+}
+normalizeFlights(flights);
