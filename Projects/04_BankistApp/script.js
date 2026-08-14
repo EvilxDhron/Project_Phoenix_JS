9ded2f5e2862234385193e38ac9fac118cdd38fa
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-08-14 14:52:51 +0530

    Projects - BankistApp DOM Elements Selected

--- Projects/04_BankistApp/script.js
+++ Projects/04_BankistApp/script.js
@@ -39,11 +39,42 @@ const accounts = [account1, account2, account3, account4];
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+/////--- DOM Elements ---/////
+
+////// State
+
+const greeting = document.querySelector(".greet");
+const currentBalanceDate = document.querySelector(".balanceDate");
+const currentBalanceTotal = document.querySelector(".totalBalance");
+const transStatus = document.querySelector(".status");
+const transStatusDate = document.querySelector(".status_date");
+const transStatusAmount = document.querySelector(".status_Amount");
+const totalAmountIN = document.querySelector(".sumAmtDeposit");
+const totalAmountOUT = document.querySelector(".sumAmtWithdraw");
+const totalAmountInterest = document.querySelector(".sumAmtInterest");
+const logoutTimer = document.querySelector(".logoutTimer");
+
+//////  Inputs
+const userIdInput = document.querySelector("#user");
+const userPinInput = document.querySelector("#pin");
+const transferUserInput = document.querySelector("#transUser");
+const transferUserAmt = document.querySelector("#transAmt");
+const loanAmtInput = document.querySelector("#loanAmt");
+const closeUserIdInput = document.querySelector(".closeUser");
+const closeUserPinInput = document.querySelector("#closePin");
+
+////// Buttons
+const navSubmitBtn = document.querySelector(".#navBtn");
+const transferUserBtn = document.querySelector("#transBtn");
+const loanBtn = document.querySelector("#loanBtn");
+const closeUserBtn = document.querySelector("#closeBtn");
+const sortTransactionBtn = document.querySelector("#sortBtn");
