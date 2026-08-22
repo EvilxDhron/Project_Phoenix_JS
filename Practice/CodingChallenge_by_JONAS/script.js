7682301b4b96ca325adca364ddc7012e63fc7e50
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2026-08-22 09:26:01 +0530

    Practice - Challenge 04 [code improved]

--- Practice/CodingChallenge_by_JONAS/script.js
+++ Practice/CodingChallenge_by_JONAS/script.js
@@ -228,16 +228,11 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 */
 
-function calcDogAge(dogAges) {
-  const humanAges = dogAges
-    .map((age) => {
-      if (age <= 2) return 2 * age;
-      else return 16 + age * 4;
-    })
-    .filter((age) => age >= 18);
-
-  const humanAgeAverage =
-    humanAges.reduce((acc, age) => acc + age, 0) / humanAges.length;
-
-  return [humanAges, humanAgeAverage];
+function calcAverageHumanAge(ages) {
+  const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter((age) => age >= 18);
+  const avgHumanAge = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return avgHumanAge;
 }
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
